cc5929d7d73e09364e977e65ecb86832685aef9b
ameya-6964 <belvalkar7@gmail.com>
2022-11-24 17:46:41 +0530

    Updated

--- Day9.java
+++ Day9.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Day9 {
-    static int digitCounter(long n) {
+    static int digitCounter(int n) {
         int count = 0;
         while (n != 0) {
             n = n / 10;
@@ -12,8 +12,8 @@ public class Day9 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please Enter Random Numbers");
-        long n = sc.nextLong();
+        System.out.println("Input Number");
+        int n = sc.nextInt();
         System.out.println("The Number Of Digits Are : " + digitCounter(n));
         sc.close();
         ;
