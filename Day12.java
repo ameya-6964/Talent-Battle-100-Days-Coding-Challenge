f8402485b3047e2f587038bf448283da29fa61ec
ameya-6964 <belvalkar7@gmail.com>
2022-11-27 18:09:36 +0530

    completed Day 12 Java

---
+++ Day12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Day12 {
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter Numbers");
+        int number = sc.nextInt();
+        for (sum = 0; number != 0; number = number / 10) {
+            sum = sum + number % 10;
+        }
+        System.out.println(sum);
+        sc.close();
+    }
+}
