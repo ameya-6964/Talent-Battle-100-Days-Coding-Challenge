88eded31a25276d4b3c018bb9377447b63e37806
ameya-6964 <belvalkar7@gmail.com>
2022-12-12 18:23:44 +0530

    Completed Day 27

---
+++ Day27.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Day27 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        System.out.println(num << 1);
+        sc.close();
+    }
+}
