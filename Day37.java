897a45359324ca3620b7f5774bb4b6a08476c767
ameya-6964 <belvalkar7@gmail.com>
2022-12-22 23:41:54 +0530

    Added Day 37

---
+++ Day37.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Day37 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = sc.nextLine();
+        int[] frequency = new int[str.length()];
+        int i, j;
+        char strnew[] = str.toCharArray();
+        for (i = 0; i < str.length(); i++) 
+        {
+            frequency[i] = 1;
+            for (j = i + 1; j < str.length(); j++) 
+            {
+                if (strnew[i] == strnew[j]) 
+                {
+                    frequency[i]++;
+                    strnew[j] = '0';
+                }
+            }
+        }
+        for (i = 0; i < frequency.length; i++) 
+        {
+            if (strnew[i] != ' ' && strnew[i] != '0') 
+            {
+                System.out.println("The frequency of " + strnew[i] + " Is " + frequency[i]);
+            }
+        }
+    }
+}
